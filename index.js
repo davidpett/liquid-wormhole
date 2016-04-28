b3a94d5d2e250e5519ef1a3f3c84b3f00e8c0ee9
David Pett <dpett@folklore.digital>
2016-04-28 08:03:18 -0500

    testing filtering

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-var filterInitializers = require('fastboot-filter-initializers');
-
 module.exports = {
-  name: 'liquid-wormhole',
-
-  preconcatTree: function(tree) {
-    return filterInitializers(tree, this.app.name);
-  }
+  name: 'liquid-wormhole'
 };
